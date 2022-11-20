ad70f5f3aa3ddd924f97179959ff7b7551d40887
Velumani M <velu_lakshmi@hotmail.com>
2022-11-20 18:00:43 +0530

    Author class commit and Spring JPA, H2 database dependency addition.

---
+++ src/main/java/com/springwebapp/wapp/library/Author.java
@@ -0,0 +1,82 @@
+package com.springwebapp.wapp.library;
+import javax.persistence.*;
+import java.util.Objects;
+import java.util.Set;
+@Entity
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String fname;
+    private String lname;
+    @ManyToMany(mappedBy = "authors")
+    private Set<Books> books;
+
+    public Author() {
+    }
+
+    public Author(String fname, String lname, Set<Books> books) {
+        this.fname = fname;
+        this.lname = lname;
+        this.books = books;
+    }
+
+    public Author(Long id) {
+        this.id = id;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public Set<Books> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Books> books) {
+        this.books = books;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", fname='" + fname + '\'' +
+                ", lname='" + lname + '\'' +
+                ", books=" + books +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(id, author.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
